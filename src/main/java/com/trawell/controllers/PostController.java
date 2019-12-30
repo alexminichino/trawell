91401ec3054e5615ac29ac9bf6c6a112b7a14836
umberto <umbertorussomando@gmail.com>
2019-12-30 18:28:20 +0100

    post, work in progess

--- src/main/java/com/trawell/controllers/PostController.java
+++ src/main/java/com/trawell/controllers/PostController.java
@@ -1,10 +1,17 @@
 package com.trawell.controllers;
 
+
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("post")
 public class PostController{
     
+    @GetMapping("/landing")
+    public String landing() {
+        return "pages/post/addPost";
+    }
 }
