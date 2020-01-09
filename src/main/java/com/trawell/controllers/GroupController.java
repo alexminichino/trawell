0732e285bedf2d125ae259b9901b13734faa013d
Renerine <milionevincent@gmail.com>
2020-01-09 12:30:42 +0100

    added mappings to group pages

--- src/main/java/com/trawell/controllers/GroupController.java
+++ src/main/java/com/trawell/controllers/GroupController.java
@@ -2,11 +2,16 @@ package com.trawell.controllers;
 
 import javax.servlet.http.HttpSession;
 
+import com.trawell.models.TrawellGroup;
 import com.trawell.models.User;
+import com.trawell.services.TrawellGroupService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -18,6 +23,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("group")
 public class GroupController {
+    @Autowired
+    TrawellGroupService dao;
+
     @GetMapping("/new-group")
     public String newGroup(HttpSession session) {
         User user = (User) session.getAttribute("user");
@@ -32,6 +40,18 @@ public class GroupController {
         if (user == null ? false : !user.getIsAdmin()) {
             
             return "pages/group/list-view";
+        }
+        return "redirect:/users/login";
+    }
+
+    @GetMapping("/view")
+    public String view(HttpSession session, @RequestParam ("id") Long id, Model model) {
+        User user = (User) session.getAttribute("user");
+        if (user == null ? false : !user.getIsAdmin()) {
+            TrawellGroup group = dao.findOne(id);
+            //model.addAttribute("posts", group.getPosts());
+            model.addAttribute("group", group);
+            return "pages/group/groupView";
         }
         return "redirect:/users/login";
     }
