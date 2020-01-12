e8af9286a1863c24eee451a4be18740b51415bfb
drak3-4598 <drak3-4598@gmail.com>
2020-01-12 16:11:38 +0100

    front end post

--- src/main/java/com/trawell/controllers/HomeController.java
+++ src/main/java/com/trawell/controllers/HomeController.java
@@ -3,6 +3,7 @@ package com.trawell.controllers;
 import javax.servlet.http.HttpSession;
 
 import com.trawell.services.CarsharingService;
+import com.trawell.services.PostService;
 import com.trawell.services.ItineraryService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,22 +14,27 @@ import org.springframework.web.bind.annotation.GetMapping;
  * Homecontroller: andranno mappate tutte le funzionalità relative ad i controller
  * @author Umberto Russomando
  */
+import org.springframework.web.bind.annotation.RequestMapping;
 
  /**
   * @author Alfieri Davide
   */
+@RequestMapping("/home")
 @Controller
 public class HomeController {
     @Autowired
     private CarsharingService daocarsharing;
     @Autowired
     private ItineraryService daoitinerary;
-    @GetMapping("/")
+    @Autowired
+    private PostService daopost;
+    @GetMapping("/home-page")
     public String landing(HttpSession session, Model model) {
         if (session.getAttribute("user") != null) {
 
             model.addAttribute("carsharingAds", daocarsharing.findAll());
             model.addAttribute("itineraries", daoitinerary.findAll());
+            model.addAttribute("posts", daopost.findAll());
 
             return "pages/home/index";
         }
